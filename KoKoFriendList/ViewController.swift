9f94841353b32884008e5a5e5c9aea288a43defa
陳逸煌 <joh1234567892001@gmail.com>
2025-10-04 13:56:07 +0800

    Initial Commit

---
+++ KoKoFriendList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KoKoFriendList
+//
+//  Created by 陳逸煌 on 2025/10/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
